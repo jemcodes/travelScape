fdad9b4eb64966fcfd98a467e6266cdde08fd5c9
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-04-07 14:25:10 -0400

    Working on the site wide nav bar.

--- routes/profiles.js
+++ routes/profiles.js
@@ -20,18 +20,36 @@ router.get('/:id(\\d+)', csrfProtection, asyncHandler(async (req, res, next) => 
     //     where: userId , 
     //     include: PenPal 
     //  });
-
     const user = await User.findByPk(userId, {
         include: [{
             model: User,
             as: 'followers'
         }]})
-
-     console.log(user.PenPals)
+        const penPalCount = user.followers.length
+     console.log(user)
     if (user) {
         console.log(user);
-        res.render('profile', { user, csrfToken: req.csrfToken() })
+        res.render('profile', { user, csrfToken: req.csrfToken(), penPalCount })
 
+    } else {
+        next(profileNotFoundError(userId));
+    }
+}))
+
+router.get('/:id(\\d+)/penpals', csrfProtection, asyncHandler(async (req, res, next) => {
+    const userId = parseInt(req.params.id, 10);
+    // const user = await User.findOne({ 
+    //     where: userId , 
+    //     include: PenPal 
+    //  });
+    const user = await User.findByPk(userId, {
+        include: [{
+            model: User,
+            as: 'followers'
+        }]})
+    //  const penpals = user.followers.map(user => user.pen_name)
+    if (user) {
+        res.render('penPals', { user, csrfToken: req.csrfToken() })
     } else {
         next(profileNotFoundError(userId));
     }
