fe2079877ffaff7e914e389e56aec7c380af59d7
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-04-07 18:55:56 -0400

    Profile pages are functional

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const db = require('../db/models');
-const { User, Article, PenPal } = db;
+const { User, Article, PenPal, Comment } = db;
 const { check, validationResult } = require('express-validator');
 const { loginUser, logoutUser, requireAuth } = require('../auth');
 const { asyncHandler, csrfProtection } = require('./utils');
@@ -21,10 +21,16 @@ router.get('/:id(\\d+)', requireAuth, csrfProtection, asyncHandler(async (req, r
         include: [{
             model: User,
             as: 'followers'
-        }]
+        }, 
+        {model: Article, include: Comment}
+        ]
     })
+    // console.log(user.Articles[0])
     const penPalCount = user.followers.length
-
+    user.Articles.forEach(article => {
+        let substring = article.content.substring(0, 200);
+        article.content = substring
+    })
 
     if (user) {
 
@@ -36,6 +42,7 @@ router.get('/:id(\\d+)', requireAuth, csrfProtection, asyncHandler(async (req, r
 }))
 
 router.get('/:id(\\d+)/penpals', requireAuth, csrfProtection, asyncHandler(async (req, res, next) => {
+    const loggedIn = req.session.auth.userId;
     const userId = parseInt(req.params.id, 10);
     // const user = await User.findOne({ 
     //     where: userId , 
@@ -48,9 +55,10 @@ router.get('/:id(\\d+)/penpals', requireAuth, csrfProtection, asyncHandler(async
         }]
     })
 
+    const penPalCount = user.followers.length
     //  const penpals = user.followers.map(user => user.pen_name)
     if (user) {
-        res.render('penPals', { user, csrfToken: req.csrfToken() })
+        res.render('penPals', { user, csrfToken: req.csrfToken(), penPalCount, loggedIn})
     } else {
         next(profileNotFoundError(userId));
     }
