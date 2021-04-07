5e48f8248761dfedca6d2cc4ebab9791e39ee48d
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-04-07 13:01:46 -0400

    Working on pug for Profiles

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const db = require('../db/models');
+const {User, Article, PenPal} = db
 const { check, validationResult } = require('express-validator');
 const { loginUser, logoutUser } = require('../auth');
 const { asyncHandler, csrfProtection } = require('./utils');
@@ -15,7 +16,18 @@ const profileNotFoundError = (id) => {
 
 router.get('/:id(\\d+)', csrfProtection, asyncHandler(async (req, res, next) => {
     const userId = parseInt(req.params.id, 10);
-    const user = await db.User.findByPk(userId);
+    // const user = await User.findOne({ 
+    //     where: userId , 
+    //     include: PenPal 
+    //  });
+
+    const user = await User.findByPk(userId, {
+        include: [{
+            model: User,
+            as: 'followers'
+        }]})
+
+     console.log(user.PenPals)
     if (user) {
         console.log(user);
         res.render('profile', { user, csrfToken: req.csrfToken() })
