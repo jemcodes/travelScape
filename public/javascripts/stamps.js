fe71bb712ecbbfd43e67742d3cff3bdf09f7f105
Patrick Nusbaum <patricknusbaum2016@gmail.com>
2021-04-08 13:02:11 -0400

    Working on the stamps API route.

--- public/javascripts/stamps.js
+++ public/javascripts/stamps.js
@@ -1,3 +1,7 @@
-// document.addEventListener('DOMContentLoaded', () => {
-
-// })
+window.addEventListener('DOMContentLoaded', (err) => {
+  const stampButton = document.getElementById("button");
+  stampButton.addEventListener("click", async (err) => {
+    err.preventDefault();
+    const articleId = document.getElementById("")
+  })
+})
