b26368ffa05ddb94e5cde79a63b48a10564cd728
Eunice Park <euunicepaark@gmail.com>
2021-04-07 20:07:26 -0400

    last in commits

--- routes/newsfeed.js
+++ routes/newsfeed.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 const { csrfProtection, asyncHandler } = require('./utils');
 const db = require('../db/models');
+const { requireAuth } = require('../auth');
 
 
 /* GET newsfeed page. */
 
-router.get('/', csrfProtection, asyncHandler(async (req, res, next) => {
+router.get('/', requireAuth, csrfProtection, asyncHandler(async (req, res, next) => {
   //NEED TO ADD ORDER BY STAMP COUNT WHEN STAMP DB IS DONE. 
   const articles = await db.Article.findAll({
     include: db.User,
